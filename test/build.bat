8a6b992c1ae44ef41ff257db6dd35345894db7c6
TNTSuperMan <tntsuperga@gmail.com>
2024-11-11 19:08:38 +0900

    del compiler(for to js)

--- test/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-..\compiler\bin\Debug\net8.0\compiler.exe main.bmm map.txt
-rem ..\assembler\bin\Debug\net8.0\assembler.exe game.bge game.bin
-rem ..\runtime\bin\Debug\net8.0-windows\runtime.exe game.bin

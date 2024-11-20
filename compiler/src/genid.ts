272ae2ec433fc7c78ba59a58f9d8e5d9c6e75015
TNTSuperMan <tntsuperga@gmail.com>
2024-11-20 18:18:32 +0900

    タグ生成の形式を変更

--- compiler/src/genid.ts
+++ compiler/src/genid.ts
@@ -1,2 +1,2 @@
 const t = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXZ0123456789".split("")
-export default ()=>"_@"+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,].map(e=>t[Math.floor(Math.random()*(t.length-1))]).join("")
+export default ()=>"_"+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,].map(e=>t[Math.floor(Math.random()*(t.length-1))]).join("")

3788fdde89497a525f66190e0a5d7262b46a73dc
TNTSuperMan <tntsuperga@gmail.com>
2024-11-25 17:01:45 +0900

    stackを移行

--- runtime/Main.cs
+++ runtime/Main.cs
@@ -3,7 +3,7 @@
     public partial class Runtime
     {
         private List<ushort> callstack = new();
-
+        private Stack stack = new();
         private Memory memory;
 
         private ushort pc = 0;
@@ -11,19 +11,6 @@
         public Runtime(byte[] rom)
         {
             memory = new Memory(rom);
-        }
-        private byte pop()
-        {
-            byte ret = stack.Last();
-            stack.RemoveAt(stack.Count - 1);
-            return ret;
-        }
-        private void push(byte data) => stack.Add(data);
-        private ushort popAddr()
-        {
-            byte bottom = pop();
-            byte top = pop();
-            return (ushort)((top << 8) | bottom);
         }
     }
 }
--- runtime/Stack.cs
+++ runtime/Stack.cs
@@ -9,5 +9,18 @@ namespace runtime
     internal class Stack
     {
         private List<byte> stack = new();
+        public byte pop()
+        {
+            byte ret = stack.Last();
+            stack.RemoveAt(stack.Count - 1);
+            return ret;
+        }
+        public void push(byte data) => stack.Add(data);
+        public ushort PopAddr()
+        {
+            byte bottom = pop();
+            byte top = pop();
+            return (ushort)((top << 8) | bottom);
+        }
     }
 }

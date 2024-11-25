95669b87a083902084e849e8ddc86532fc54b455
TNTSuperMan <tntsuperga@gmail.com>
2024-11-25 17:10:40 +0900

    イベント系

---
+++ runtime/Delegates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace runtime
+{
+    public delegate void Redraw();
+    public delegate byte GetKeyState();
+    public delegate void Save(byte[] data);
+    public delegate byte[] Load();
+    public delegate void Sound(byte hz, byte len);
+    public delegate void StopSound();
+
+    public partial class Runtime
+    {
+        public Redraw onRedraw;
+        public GetKeyState onKeyState;
+        public Save onSave;
+        public Load onLoad;
+        public Sound onSound;
+        public StopSound onStopSound;
+    }
+}

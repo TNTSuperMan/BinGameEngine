eaca3de9f204cd8e38ae10a79e0487ff6bbb49d3
TNTSuperMan <tntsuperga@gmail.com>
2024-11-27 22:15:26 +0900

    graphic

--- bgedit/src/App.tsx
+++ bgedit/src/App.tsx
@@ -1,9 +1,12 @@
 import Menu from "./components/FileMenu"
+import Graphic from "./components/Graphic"
 
 function App() {
   return (
     <>
       <Menu/>
+      <Graphic graph={[0b110000,0b1100,0b11,0b01000000,0b111111,128,
+                       0b110011,0b1111,0b111100]} size={1} />
     </>
   )
 }
--- bgedit/src/components/Graphic.tsx
+++ bgedit/src/components/Graphic.tsx
@@ -1,26 +1,33 @@
-function Pixel(props: {x:number, y:number, d:number, size:number}){
+import "./Graphic.scss";
+
+function Pixel(props: {x:number, y:number, d:number, size:number, i:number}){
     if((props.d & 0b11000000) >> 6 == 0b01){
-        return <></>
+        return <div className="pixel" key={props.i} style={{
+            width: props.size,
+            height:props.size
+        }}/>
     }else{
-        return <div className="pixel" style={{
+        return <div className="pixel" key={props.i} style={{
             left:props.x * props.size,
             top: props.y * props.size,
             width: props.size,
-            height:props.size
+            height:props.size,
+            background: `rgb(${((props.d&0b110000)>>4)*85},${((props.d&0b1100)>>2)*85},${(props.d&0b11)*85})`
         }}/>
     }
 }
 
 function Graphic(props: {graph:number[], size:number}){
     const pixels:JSX.Element[] = [];
-    let x = 0,y = 0;
+    let x = 0,y = 0,i = 0;
     props.graph.forEach(d=>{
-        pixels.push(Pixel({x,y,d,size:props.size}));
-        x++;
-        if((d & 0b110000) >> 6 == 0b10){
+        if((d & 0b11000000) >> 6 == 0b10){
             x = 0;
             y++;
         }
+        pixels.push(Pixel({x,y,d,size:props.size,i}));
+        x++;
+        i++;
     })
     return <div className="graph">{pixels}</div>;
 }

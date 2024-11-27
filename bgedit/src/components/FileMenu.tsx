8896a529460abf8274c3c3061e4b8801d1328edb
TNTSuperMan <tntsuperga@gmail.com>
2024-11-27 21:48:07 +0900

    ファイル操作系の追加

--- bgedit/src/components/FileMenu.tsx
+++ bgedit/src/components/FileMenu.tsx
@@ -18,21 +18,27 @@ function Menu(){
     const dispatch = useDispatch();
     const downloader = useRef<HTMLAnchorElement|null>(null);
     const uploader = useRef<HTMLInputElement|null>(null);
+    function download(data:string, mime:string, link:string){
+        if(downloader.current){
+            const url = URL.createObjectURL(
+                new Blob([data], {type: mime}));
+            downloader.current.href = url;
+            downloader.current.download = link;
+            downloader.current.click();
+            setTimeout(()=>URL.revokeObjectURL(url), 5000);
+        }
+    }
     return (<>
         <MenuBtn text="新規" func={()=>dispatch(init(1))}/>
         <MenuBtn text="開く" func={()=>uploader.current?.click()}/>
-        <MenuBtn text="保存" func={()=>{
-            if(downloader.current){
-                const url = URL.createObjectURL(
-                    new Blob([JSON.stringify(store.getState().data)],
-                    {type: 'application/json'}));
-                downloader.current.href = url;
-                downloader.current.download = "graphics.json";
-                downloader.current.click();
-                setTimeout(()=>URL.revokeObjectURL(url), 5000)
-            }
-        }}/>
-        <MenuBtn text="エクスポート" func={()=>0}/>
+        <MenuBtn text="保存" func={()=>download(
+            JSON.stringify(store.getState().data),
+            "application/json", "graphics.json")}/>
+        <MenuBtn text="エクスポート" func={()=>download(
+            store.getState().data.data.map(e=>
+                e.data.map(e=>String.fromCharCode(e)).join("")).join(""),
+            "application/octet-stream", "graphics.bin"
+        )}/>
         <a ref={downloader} style={{display:"none"}} target="_blank"/>
         <input ref={uploader} type="file" style={{display:"none"}}
             onChange={e=>e.target.files?.item(0)?.text().then(e=>dispatch(open(e)))} />

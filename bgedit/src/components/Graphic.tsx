46088b323e0fcf75cdbfbb255d0c7ba8d9f3a0d3
TNTSuperMan <tntsuperga@gmail.com>
2024-11-27 22:17:40 +0900

    g2

--- bgedit/src/components/Graphic.tsx
+++ bgedit/src/components/Graphic.tsx
@@ -1,20 +1,14 @@
 import "./Graphic.scss";
 
 function Pixel(props: {x:number, y:number, d:number, size:number, i:number}){
-    if((props.d & 0b11000000) >> 6 == 0b01){
-        return <div className="pixel" key={props.i} style={{
-            width: props.size,
-            height:props.size
-        }}/>
-    }else{
-        return <div className="pixel" key={props.i} style={{
-            left:props.x * props.size,
-            top: props.y * props.size,
-            width: props.size,
-            height:props.size,
-            background: `rgb(${((props.d&0b110000)>>4)*85},${((props.d&0b1100)>>2)*85},${(props.d&0b11)*85})`
-        }}/>
-    }
+    return <div className="pixel" key={props.i} style={{
+        left:props.x * props.size,
+        top: props.y * props.size,
+        width: props.size,
+        height:props.size,
+        background: (props.d & 0b11000000) >> 6 == 0b01 ? "" :
+        `rgb(${((props.d&0b110000)>>4)*85},${((props.d&0b1100)>>2)*85},${(props.d&0b11)*85})`
+    }}/>
 }
 
 function Graphic(props: {graph:number[], size:number}){

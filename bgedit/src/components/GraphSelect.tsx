efb54d728dacafd244d488c1a59609b62b56ed5b
TNTSuperMan <tntsuperga@gmail.com>
2024-11-28 13:10:25 +0900

    GraphSelectの型と大きさを変える

--- bgedit/src/components/GraphSelect.tsx
+++ bgedit/src/components/GraphSelect.tsx
@@ -1,12 +1,12 @@
 import "./GraphSelect.scss"
 import Graphic from "./Graphic";
 
-function GraphSelect(props: {data:{name:string, data:number[]}[], setid: (id:number)=>void}){
+function GraphSelect(props: {data:{name:string, data:number[][]}[], setid: (id:number)=>void}){
     return <div className="graphselect">
         {props.data.map((e,i)=>
             <div className="graphitem" key={i} onClick={()=>props.setid(i)}>
                 <span>{e.name}</span>
-                <Graphic graph={e.data} size={1} onclick={()=>{}}/>
+                <Graphic graph={e.data} size={2} onclick={()=>{}}/>
         </div>)}
     </div>
 }

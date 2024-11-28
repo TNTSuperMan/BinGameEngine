e4976b889c7f4bd93ae650bae636d576bf34be05
TNTSuperMan <tntsuperga@gmail.com>
2024-11-28 16:40:56 +0900

    サイズの残りを表示する

--- bgedit/src/components/Menu.tsx
+++ bgedit/src/components/Menu.tsx
@@ -1,9 +1,11 @@
 import "./Menu.scss";
-import FileMenu from "./FileMenu";
+import FileMenu, { ExportData } from "./FileMenu";
 
 function Menu(){
+    const explen = ExportData().length;
     return <div className="menu">
         <FileMenu/>
+        長さ：{explen}, 残り：{0x1000 - explen}
     </div>
 }
 

7458455dcd9d55ad6221fdd1a3b0d3313120c410
TNTSuperMan <tntsuperga@gmail.com>
2024-11-28 11:41:47 +0900

    パレットのを修正

--- bgedit/src/components/Palette.tsx
+++ bgedit/src/components/Palette.tsx
@@ -5,11 +5,11 @@ function Palette(props: {changeState: (c:Color)=>void}){
     const [color, changeColor] = useState<Color>({R:0,G:0,B:0,isTransparent:false});
     useEffect(()=>props.changeState(color), [color]);
     return <div className="palette">
-        <input type="range" name="R" value={color.R} onChange={e=>
+        <input type="range" name="R" value={color.R} min={0} max={3} onChange={e=>
             changeColor({R:parseInt(e.target.value), G:color.G, B:color.B, isTransparent: false})} />
-        <input type="range" name="G" value={color.R} onChange={e=>
+        <input type="range" name="G" value={color.G} min={0} max={3} onChange={e=>
             changeColor({R:color.R, G:parseInt(e.target.value), B:color.B, isTransparent: false})} />
-        <input type="range" name="B" value={color.R} onChange={e=>
+        <input type="range" name="B" value={color.B} min={0} max={3} onChange={e=>
             changeColor({R:color.R, G:color.G, B:parseInt(e.target.value), isTransparent: false})} />
     </div>
 }

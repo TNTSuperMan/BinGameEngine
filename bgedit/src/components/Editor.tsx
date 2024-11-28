424b1c94004596596a9d1c7a02261f79a44c4574
TNTSuperMan <tntsuperga@gmail.com>
2024-11-28 10:49:30 +0900

    定数を使う

--- bgedit/src/components/Editor.tsx
+++ bgedit/src/components/Editor.tsx
@@ -2,13 +2,16 @@ import { useDispatch, useSelector } from "react-redux";
 import "./Editor.scss";
 import { edit, Graphic, State } from "../store";
 
+const TRANSPARENT = 0b01000000;
+const NEXTLINE = 0b10000000;
+
 function Editor({index}: {index:number}){
     const data = useSelector<{data:State}, Graphic[]>(e=>e.data.data);
     const dispatch = useDispatch();
     if(index == -1){
         return <></>
     }else{
-        const isNextLine = (e:number) => (0b11000000 & e) >> 6 == 0b10;
+        const isNextLine = (e:number) => (0b10000000 & e) == NEXTLINE;
         const lineat = data[index].data.findIndex(isNextLine);
         const width = lineat == -1 ? data[index].data.length : lineat;
         const height= data[index].data.filter(isNextLine).length + 1;
@@ -25,7 +28,7 @@ function Editor({index}: {index:number}){
                         if(isNextLine(e) || i == data[index].data.length-1){
                             if(widthDiff > 0){
                                 for(let i = 0;i < widthDiff;i++)
-                                    after.push(0b10000000);
+                                    after.push(TRANSPARENT);
                             }else{
                                 for(let i = 0;i < (0-widthDiff);i++)
                                     after.pop();
@@ -40,8 +43,8 @@ function Editor({index}: {index:number}){
                 <input type="number" value={height} onChange={e=>{
                     if(Number.isNaN(parseInt(e.target.value))) return;
                     const addition:number[] = [];
-                    for(let i = 0;i < parseInt(e.target.value);i++)
-                        addition.push(0b10000000);
+                    for(let i = 0;i < parseInt(e.target.value)+1;i++)
+                        addition.push(i == 0 ? NEXTLINE : TRANSPARENT);
                     dispatch(edit([index, [...data[index].data, ...addition]]));
                 }} />
             </div>

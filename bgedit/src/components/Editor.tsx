827ca7bae3d41e28fec67f65e7f14d49d82a6bf6
TNTSuperMan <tntsuperga@gmail.com>
2024-11-28 13:29:35 +0900

    ズームボタン

--- bgedit/src/components/Editor.tsx
+++ bgedit/src/components/Editor.tsx
@@ -11,13 +11,14 @@ function Editor({index}: {index:number}){
     const data = useSelector<{data:State}, GraphData[]>(e=>e.data.data);
     const dispatch = useDispatch();
     const [color,changeColor] = useState<Color>({R:0,G:0,B:0,isTransparent:false});
+    const [zoom, changeZoom] = useState(5);
     if(index == -1){
         return <></>
     }else{
         return <>
-            <EditorMenu index={index}/>
+            <EditorMenu index={index} changeZoom={changeZoom}/>
             <div className="editor">
-                <Graphic graph={data[index].data} size={5} onclick={(x,y)=>{
+                <Graphic graph={data[index].data} size={zoom} onclick={(x,y)=>{
                     if(color.isTransparent){
                         dispatch(editPixel([index, x, y, TRANSPARENT]));
                     }else{
--- bgedit/src/components/EditorMenu.tsx
+++ bgedit/src/components/EditorMenu.tsx
@@ -2,46 +2,55 @@ import "./EditorMenu.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { edit, Graphic, State } from "../store";
 import { TRANSPARENT } from "./_editorutil";
+import { useEffect, useState } from "react";
 
-function EditorMenu({index}: {index:number}){
+function EditorMenu(props: {index:number, changeZoom: (v:number)=>void}){
     const dispatch = useDispatch();
+    const index = props.index;
     const data = useSelector<{data:State}, Graphic[]>(e=>e.data.data);
-    const width = data[index].data[0]?.length ?? 0;
-    const height= data[index].data.length;
+    const width = data[props.index].data[0]?.length ?? 0;
+    const height= data[props.index].data.length;
+
+    const [zoom, changezoom] = useState(5);
+    useEffect(()=>props.changeZoom(zoom), [zoom, props]);
 
     return <div className="editorMenu">
         <span>{data[index]?.name}</span>
-        <input type="number" value={width} onChange={e=>{
-            if(Number.isNaN(parseInt(e.target.value))) return;
-            const after:number[][] = [...data[index].data];
-            const widthDiff = parseInt(e.target.value) - width;
-            if(widthDiff > 0){
-                for(let i = 0;i < widthDiff;i++)
-                    for(let j = 0;j < after.length;j++)
-                        after[j].push(TRANSPARENT);
-            }else{
-                for(let i = 0;i < -widthDiff;i++)
-                    for(let j = 0;j < after.length;j++)
-                        after[j].pop();
-            }
-            dispatch(edit([index, after]));
-        }}/>
-        <span className="x">x</span>
-        <input type="number" value={height} onChange={e=>{
-            if(Number.isNaN(parseInt(e.target.value))) return;
-            const heightDiff = parseInt(e.target.value) - height;
-            if(height > 0){
-                const addition:number[] = [];
-                for(let i = 0;i < heightDiff;i++)
-                    addition.push(TRANSPARENT);
-                dispatch(edit([index, [...data[index].data, ...addition]]));
-            }else{
+        <div className="size">
+            <input type="number" value={width} onChange={e=>{
+                if(Number.isNaN(parseInt(e.target.value))) return;
                 const after:number[][] = [...data[index].data];
-                for(let i = 0;i < -heightDiff;i++)
-                    after.pop();
+                const widthDiff = parseInt(e.target.value) - width;
+                if(widthDiff > 0){
+                    for(let i = 0;i < widthDiff;i++)
+                        for(let j = 0;j < after.length;j++)
+                            after[j].push(TRANSPARENT);
+                }else{
+                    for(let i = 0;i < -widthDiff;i++)
+                        for(let j = 0;j < after.length;j++)
+                            after[j].pop();
+                }
                 dispatch(edit([index, after]));
-            }
-        }} />
+            }}/>
+            <span className="x">x</span>
+            <input type="number" value={height} onChange={e=>{
+                if(Number.isNaN(parseInt(e.target.value))) return;
+                const heightDiff = parseInt(e.target.value) - height;
+                if(height > 0){
+                    const addition:number[] = [];
+                    for(let i = 0;i < heightDiff;i++)
+                        addition.push(TRANSPARENT);
+                    dispatch(edit([index, [...data[index].data, ...addition]]));
+                }else{
+                    const after:number[][] = [...data[index].data];
+                    for(let i = 0;i < -heightDiff;i++)
+                        after.pop();
+                    dispatch(edit([index, after]));
+                }
+            }}/>
+        </div>
+
+        zoom:<input type="number" max={100} min={1} name="zoom" value={zoom} onChange={e=>changezoom(parseInt(e.target.value))} />
     </div>
 }
 

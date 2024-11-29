b2f961887752556e9b7614dd25e68a464c1e2a35
TNTSuperMan <tntsuperga@gmail.com>
2024-11-29 22:46:50 +0900

    コンパイラ：描画系関数

--- compiler/src/native.ts
+++ compiler/src/native.ts
@@ -20,3 +20,6 @@ export const equal=  (a:Expr,b:Expr):Expr=>a+b+"/ equal\n";
 export const greater=(a:Expr,b:Expr):Expr=>a+b+"/ greater\n";
 export const set=(a:Variable,b:Expr):Expr=>b+varaddr(a)+"/ store\n";
 export const ret=(...a:Expr[]):Expr=>a.join("\n")+"/ ret\n";
+
+export const rect=(x:Expr,y:Expr,w:Expr,h:Expr,c:Expr):Expr=>x+y+w+h+c+"/ rect\n";
+export const redraw=():Expr=>"/ redraw\n";

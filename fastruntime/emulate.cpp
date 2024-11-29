605671fb0689bf7b595a1948ba74ea0047b4922f
TNTSuperMan <tntsuperga@gmail.com>
2024-11-29 18:54:59 +0900

    演算命令の実装

--- fastruntime/emulate.cpp
+++ fastruntime/emulate.cpp
@@ -19,11 +19,45 @@ void Runtime::EmulateFrame() {
 }
 
 void Runtime::Emulate() {
+    uchar tmp;
     switch (Load(pc)) {
     case nop:
         break;
     case push:
         Push(Load(++pc));
+        break;
+    case pop:
+        Pop();
+        break;
+    case cls:
+        stack_count = 0;
+        break;
+    case add:
+        Push(Pop() + Pop());
+        break;
+    case sub:
+        tmp = Pop();
+        Push(Pop() + tmp);
+        break;
+    case mul:
+        Push(Pop() * Pop());
+        break;
+    case div:
+        tmp = Pop();
+        Push(Pop() / tmp);
+        break;
+    case rem:
+        tmp = Pop();
+        Push(Pop() % tmp);
+        break;
+    case nand:
+        Push(~(Pop() & Pop()));
+        break;
+    case equal:
+        Push(Pop() == Pop() ? 1 : 0);
+        break;
+    case greater: //スタックの都合上逆です。
+        Push(Pop() < Pop() ? 1 : 0);
         break;
     }
 }

13caee35d54dcf7195a26197256b53feba126470
TNTSuperMan <tntsuperga@gmail.com>
2024-11-29 22:47:08 +0900

    コンパイラ：shortutilをデフォルトで読まないようにする

--- compiler/src/shortutil.ts
+++ compiler/src/shortutil.ts
@@ -5,29 +5,31 @@ import { Variable, defvar as defBvar, varaddr, vr } from "var";
 
 export type ShortVar = [Variable, Variable];
 
-export const defvar = ():ShortVar => 
-    [defBvar(), defBvar()];
-
-export const Store = (v:ShortVar):Expr =>
-    `/ ${v[1]} store ${v[0]} store\n`;
-
-export const Load = (v:ShortVar):Expr => 
-    vr(v[0]) + vr(v[1])
-
-export const Addr = (v:ShortVar):Expr => 
-    varaddr(v[0]) + varaddr(v[1]);
-
-
-export const Add = defn<[Expr,Expr,Expr]>("su_add",(addr0,addr1,expr)=>[
-    If(greater(vr(expr), add(sub(vr(addr1)+"\n/ load\n",num(255)),num(1))),[ // expr > 256-short1 繰り上げ
-        set(addr0, add(vr(addr0), num(1)))
+export default ()=>({
+    defvar:():ShortVar => 
+        [defBvar(), defBvar()],
+    
+    Store:(v:ShortVar):Expr =>
+        (`/ ${v[1]} store ${v[0]} store\n`),
+    
+    Load:(v:ShortVar):Expr => 
+        vr(v[0]) + vr(v[1]),
+    
+    Addr:(v:ShortVar):Expr => 
+        varaddr(v[0]) + varaddr(v[1]),
+    
+    
+    Add:defn<[Expr,Expr,Expr]>("su_add",(addr0,addr1,expr)=>[
+        If(greater(vr(expr), add(sub(vr(addr1)+"\n/ load\n",num(255)),num(1))),[ // expr > 256-short1 繰り上げ
+            set(addr0, add(vr(addr0), num(1)))
+        ]),
+        set(addr1, add(vr(addr1), vr(expr)))
     ]),
-    set(addr1, add(vr(addr1), vr(expr)))
-])
-
-export const Sub = defn<[Expr,Expr,Expr]>("su_sub",(addr0,addr1,expr)=>[
-    If(greater(vr(expr), vr(addr1)+"\n/ load\n"),[ // expr > 256-short1 繰り上げ
-        set(addr0, sub(vr(addr0), num(1)))
-    ]),
-    set(addr1, sub(vr(addr1), vr(expr)))
-])
+    
+    Sub:defn<[Expr,Expr,Expr]>("su_sub",(addr0,addr1,expr)=>[
+        If(greater(vr(expr), vr(addr1)+"\n/ load\n"),[ // expr > 256-short1 繰り上げ
+            set(addr0, sub(vr(addr0), num(1)))
+        ]),
+        set(addr1, sub(vr(addr1), vr(expr)))
+    ])
+})

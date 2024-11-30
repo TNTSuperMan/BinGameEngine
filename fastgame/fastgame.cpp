890b9a461a647c554842bf8cc5e4224ba86c6064
TNTSuperMan <tntsuperga@gmail.com>
2024-11-30 17:35:14 +0900

    fastgameのを書く

--- fastgame/fastgame.cpp
+++ fastgame/fastgame.cpp
@@ -5,8 +5,14 @@
 #include <windows.h>
 
 #include <fstream>
+#include <vector>
 
 #include "DxLib.h"
+#include "fastruntime.hpp"
+
+using namespace std;
+
+typedef unsigned char uchar;
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -20,6 +26,41 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     SetDrawScreen(DX_SCREEN_BACK);
 
     std::ifstream ifs;
-    ifs.open("")
+    ifs.open(".\\out.bin");
+    if(!ifs) return -2;
+    vector<uchar> data;
+    char c;
+    while((c = ifs.get()) != EOF)
+        data.push_back(c);
+    Runtime vm = Runtime(data.data(), data.size());
+    bool isEnded = false;
+    vector<Pect> disps;
+    Pect cur = Pect(0,0,0);
+    vm.onEnd = [&isEnded](){isEnded = true;};
+    vm.onRedraw = [&disps](vector<Pect> d){
+        disps.clear();
+        for(int i = 0;i < d.size();i++)
+            disps.push_back(d[i]);
+    };
+    while(ProcessMessage() == 0 && !isEnded){
+        vm.EmulateFrame();
+        ClearDrawScreen();
+        for(int i = 0;i < disps.size();i++){
+            cur = disps[i];
+            if(cur.isDraw){
+                if(cur.isPixel){
+                    DrawPixel(cur.X, cur.Y, GetColor(cur.R, cur.G, cur.B));
+                }else{
+                    DrawBox(
+                        cur.X, cur.Y,
+                        cur.X + cur.Width,
+                        cur.Y + cur.Height,
+                        GetColor(cur.R, cur.G, cur.B), 1);
+                }
+            }
+        }
+        ScreenFlip();
+    }
+    DxLib_End();
     return 0;
 }

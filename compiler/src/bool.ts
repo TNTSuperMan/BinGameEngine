dcd4225e1321e0c1e1f22d1affb2f982be76dc45
TNTSuperMan <tntsuperga@gmail.com>
2024-12-03 13:07:06 +0900

    orのコードしゅうせい　

--- compiler/src/bool.ts
+++ compiler/src/bool.ts
@@ -2,5 +2,5 @@ import { Expr } from "./native.ts";
 
 export const boolify = (a:Expr):Expr => a+"/ 0 greater\n";
 export const not = (a:Expr):Expr => a+"/ 0 equal\n";
-export const or = (a:Expr, b:Expr):Expr => boolify(a)+boolify(b)+"/ add 1 greater\n"
+export const or = (a:Expr, b:Expr):Expr => boolify(a)+boolify(b)+"/ add 0 greater\n"
 export const and = (a:Expr, b:Expr):Expr => not(a+b+"/ nand\n")

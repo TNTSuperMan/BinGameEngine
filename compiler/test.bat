8c970a7d186ac9bbfad4fa0850d35c24ae656318
TNTSuperMan <tntsuperga@gmail.com>
2024-12-03 22:35:40 +0900

    テスト用ランタイムとか

---
+++ compiler/test.bat
@@ -0,0 +1,2 @@
+..\assembler\bin\Debug\net8.0\assembler.exe tmp.bge tmp.bin
+..\testruntime\bin\Debug\net8.0\testruntime.exe tmp.bin tmp.json
---
+++ testruntime/Program.cs
@@ -0,0 +1,17 @@
+﻿using bgeruntime;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("usage: testruntime [srcbin] [outpath]");
+            return;
+        }
+        Runtime vm = new Runtime(File.ReadAllBytes(args[0]));
+        vm.EmulateFrame();
+        File.WriteAllText(args[1], JsonSerializer.Serialize(vm.debug.StackList.ToList()));
+    }
+}

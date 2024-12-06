8e453b14ee1da29da0cbef4127fe3412e595b4fd
TNTSuperMan <tntsuperga@gmail.com>
2024-12-07 00:38:34 +0900

    やっぱユーザーが任意に出力させる方隙にする

--- bgecompiler/src/index.ts
+++ bgecompiler/src/index.ts
@@ -1,6 +1,5 @@
 import { Exprs } from "./control.ts"
 import { bindefines, fndefines } from "./fn.ts"
-import { echoVars } from "./var.ts"
 
 export * from "./bool.ts"
 export * from "./control.ts"
@@ -8,7 +7,4 @@ export * from "./fn.ts"
 export * from "./native.ts"
 export * from "./shortutil.ts";
 export * from "./var.ts"
-export const Bundle = (entry: Exprs) => {
-    echoVars();
-    return entry.join("") + fndefines + "\n" + bindefines;
-}
+export const Bundle = (entry: Exprs) => entry.join("") + fndefines + "\n" + bindefines;

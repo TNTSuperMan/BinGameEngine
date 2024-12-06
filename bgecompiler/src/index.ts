c786848acdfbfd7bd2e3bab803f899267a5aff96
TNTSuperMan <tntsuperga@gmail.com>
2024-12-07 00:38:09 +0900

    変数表示機能

--- bgecompiler/src/index.ts
+++ bgecompiler/src/index.ts
@@ -1,5 +1,6 @@
 import { Exprs } from "./control.ts"
 import { bindefines, fndefines } from "./fn.ts"
+import { echoVars } from "./var.ts"
 
 export * from "./bool.ts"
 export * from "./control.ts"
@@ -7,4 +8,7 @@ export * from "./fn.ts"
 export * from "./native.ts"
 export * from "./shortutil.ts";
 export * from "./var.ts"
-export const Bundle = (entry: Exprs) => entry.join("") + fndefines + "\n" + bindefines;
+export const Bundle = (entry: Exprs) => {
+    echoVars();
+    return entry.join("") + fndefines + "\n" + bindefines;
+}
--- bgecompiler/src/var.ts
+++ bgecompiler/src/var.ts
@@ -1,5 +1,7 @@
 import { Expr } from "./native.ts";
 
+const vars: [string, number][] = [];
+
 export const varaddr = (a:Variable):Expr=>{
     let h:string = a.toString(16)
     let t = "/ ";
@@ -14,10 +16,14 @@ export const vr =(a:Variable)=>varaddr(a)+"/ load\n";
 let i:Variable = 0xa000;
 
 export type Variable = number;
-export const defvar = ():Variable => {
+export const defvar = (description?: string):Variable => {
     if(i >= 0xffff){
         throw new RangeError("Too many vars")
     }else{
+        if(description) vars.push([description, i]);
         return i++;
     }
 }
+
+export const echoVars = () =>
+    vars.forEach(e=>console.log(`[Var]${e[0]}: ${e[1].toString(16)}`))

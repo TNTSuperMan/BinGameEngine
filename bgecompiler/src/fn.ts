4a2f1f514e4ec43fb2d3a5fd1e71045a919319e1
TNTSuperMan <tntsuperga@gmail.com>
2025-01-18 21:50:50 +0900

    genid非依存にする

--- bgecompiler/src/fn.ts
+++ bgecompiler/src/fn.ts
@@ -1,7 +1,7 @@
 import { defvar, toptr, Variable } from "./var";
 import { Expr } from "./native";
 import { Exprs } from "./control"
-import genid from "./genid";
+export const tags: Set<string> = new Set;
 export let  fndefines = "";
 export let bindefines = "";
 export let resdefines = "";
@@ -9,7 +9,8 @@ export let resdefines = "";
 export const defn = <T extends string[]>(name:string, fn:(...vars:number[])=>Exprs):(...args:T)=>Expr => {
     let vars:Variable[] = [];
     for(let i = 0;i < fn.length;i++) vars.push(defvar());
-    const realname = ":fn_" +name + genid();
+    const realname = ":fn_" +name;
+    if(tags.has(realname)) throw new ReferenceError("Already defined function: "+name);
 
     fndefines += realname + "\n";
     fndefines += vars.reverse().map(e=>toptr(e).join("")+"/ store\n").join("")
@@ -18,7 +19,8 @@ export const defn = <T extends string[]>(name:string, fn:(...vars:number[])=>Exp
 }
 
 export const loadbinary = (name: string, path: string):Expr => {
-    const realname = ":binary_" + name + genid();
+    const realname = ":binary_" + name;
+    if(tags.has(realname)) throw new ReferenceError("Already defined binary: "+name);
     bindefines += realname;
     bindefines += `
 inject ${path}\n`
@@ -26,14 +28,16 @@ inject ${path}\n`
 }
 
 export const defres = (name: string, resource: string):Expr => {
-    const realname = ":" + name + genid();
+    const realname = ":" + name;
+    if(tags.has(realname)) throw new ReferenceError("Already defined resource: "+name);
     resdefines += realname + "\n";
     resdefines += "inject_fromB64 " + btoa(resource) + "\n";
     return "/ " + realname + "\n";
 }
 
 export const defExprRes = (name: string, res: Expr):Expr => {
-    const realname = ":" + name + genid();
+    const realname = ":" + name;
+    if(tags.has(realname)) throw new ReferenceError("Already defined exprres: "+name);
     resdefines += realname + "\n";
     resdefines += res;
     return "/ " + realname + "\n";

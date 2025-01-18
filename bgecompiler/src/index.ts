982111c270dedbf0557a06cef7722189e623f9de
TNTSuperMan <tntsuperga@gmail.com>
2025-01-18 22:29:39 +0900

    genidを公開

--- bgecompiler/src/index.ts
+++ bgecompiler/src/index.ts
@@ -1,11 +1,13 @@
 import { Exprs } from "./control"
+import genid from "./genid"
 import { bindefines, fndefines, resdefines } from "./fn"
 
 export * from "./bit"
 export * from "./bool"
 export * from "./control"
 export * from "./fn"
+export { genid }
 export * from "./native"
-export * from "./shortutil";
+export * from "./shortutil"
 export * from "./var"
 export const Bundle = (entry: Exprs) => entry.join("") + fndefines + resdefines + bindefines;
